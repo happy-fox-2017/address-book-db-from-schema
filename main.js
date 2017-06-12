482cf1792eb1c27afd84ca7d4fbbc0302e4ee812
Adith <adithwidyapradipta@gmail.com>
2017-06-12 08:29:26 +0700

    pull

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 
+const Contact = require('./contact.js');
+const Group = require('./group.js');
+const ContactGroup = require('./contact-group.js');
+
+const repl = require('repl');
+const replServer = repl.start({
+  prompt: ">> ",
+  input: process.stdin,
+  output: process.stdout
+})
+
+replServer.context.Contact = Contact;
+replServer.context.Group = Group;
+replServer.context.ContactGroup = ContactGroup;
